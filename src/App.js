dd65d76c5cc4abf015e681e521b580d8cd3906e8
hkcwby <alexanderstevenson@me.com>
2022-12-28 16:10:22 -0600

    basic setup of alphavantage API key

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import './App.css';
 
 function App() {
+
+const apiKey = process.env.ALPHA_VANTAGE_KEY;
+
+// const request = require("request");
+
+// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+console.log(fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=${apiKey}`))
+    // .JSONParse()                                   
+    // .then(() => console.log("success"));
+  
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         A fun little application exploring the Alpha Vantage API
         </p>
+        <p></p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.alphavantage.co/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out the API here
         </a>
       </header>
     </div>
