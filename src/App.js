4e6f929719fd864513485f8fc5f73ff2cb44ebcf
hkcwby <alexanderstevenson@me.com>
2022-12-29 02:34:20 -0600

    added prettier

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const apiKey = process.env.ALPHA_VANTAGE_KEY;
 
-const apiKey = process.env.ALPHA_VANTAGE_KEY;
+  // const request = require("request");
 
-// const request = require("request");
+  // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+  console.log(
+    fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=${apiKey}`
+    )
+  );
+  // .JSONParse()
+  // .then(() => console.log("success"));
 
-// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-console.log(fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=${apiKey}`))
-    // .JSONParse()                                   
-    // .then(() => console.log("success"));
-  
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-         A fun little application exploring the Alpha Vantage API
-        </p>
+        <p>A fun little application exploring the Alpha Vantage API</p>
         <p></p>
         <a
           className="App-link"
