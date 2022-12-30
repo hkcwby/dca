745856915f5e6c7762f2ddcf6d896f1d5f7dec8d
hkcwby <alexanderstevenson@me.com>
2022-12-29 23:34:01 -0600

    experimenting with axios for making requests to API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "./AxiosSetup";
 
 function App() {
-  const apiKey = process.env.ALPHA_VANTAGE_KEY;
+  const apiKey = process.env.REACT_APP_ALPHA_VANTAGE_KEY;
+  console.log(apiKey);
+  const currency = "USD";
+  const endpoints = {
+    BTCUSD: `query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=${currency}&apikey=${apiKey}`,
+  };
 
-  // const request = require("request");
+  const [price, setPrice] = useState();
+  useEffect(() => {
+    async function dataFetch() {
+      await axios
+        .get(endpoints.BTCUSD)
+        .then((response) =>
+          console.log(
+            response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+          )
+        )
+        .catch((error) => console.log(error));
+    }
+    dataFetch();
+  }, []);
 
-  // replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-  console.log(
-    fetch(
-      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=${apiKey}`
-    )
-  );
-  // .JSONParse()
-  // .then(() => console.log("success"));
+  // fetch(
+  //   `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=${apiKey}`
+  // )
+  //   .then((response) => response.JSONPARSE)
+  //   .then((data) => console.log(data))
+  //   .then(console.log("success"));
 
   return (
     <div className="App">
