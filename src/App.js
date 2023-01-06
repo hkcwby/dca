d90dd0549d20222f778664d54977711dd14bac1c
hkcwby <alexanderstevenson@me.com>
2023-01-06 11:02:58 -0600

    added a loading transition during API data request

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
   const currency = "USD";
   const endpoints = {
     current: `query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=${currency}&apikey=${apiKey}`,
-    weekly: `query?function=DIGITAL_CURRENCY_WEEKLY&symbol=BTC&market=${currency}&apikey=${apiKey}`,
+    weekly: `query?function=DIGITAL_CURRENCY_WEEKLY&symbol=BTC&market=${currency}&apikey=${apiKey}&outputsize=compact`,
   };
+  const [loading, setLoading] = useState(true);
   //the current price
   const [price, setPrice] = useState(0);
   //intraday variables
@@ -16,55 +17,83 @@ function App() {
   const [dayPrice, setDayPrice] = useState();
   //weekly price variables
   const [week, setWeek] = useState();
-  const [weekPrices, setWeekPrices] = useState();
-  const [weeks, setWeeks] = useState();
+  const [weekPrices, setWeekPrices] = useState({
+    "1a. open (USD)": 0,
+    "4a. close (USD)": 0,
+    "2a. high (USD)": 0,
+    "3a. low (USD)": 0,
+  });
+
+  const [weeks, setWeeks] = useState([0]);
   const [weeklyPriceData, setWeeklyPriceData] = useState();
 
   //monthly price variables
-  const [month, setMonth] = useState();
-  const [monthPrice, setMonthPrice] = useState();
-
-  async function dataFetchWeekly() {
-    await axios
-      .get(endpoints.weekly)
-      .then((response) => {
-        setWeeklyPriceData(
-          response.data["Time Series (Digital Currency Weekly)"]
-        );
-        let dates = Object.keys(
-          response.data["Time Series (Digital Currency Weekly)"]
-        );
-        setWeeks(dates);
-        //console.log("this is weekly data", weeklyPriceData[weeks[0]]);
-        setWeekPrices(weeklyPriceData[weeks[0]]);
-        console.log("weekly useState", weeklyPriceData[weeks[0]]);
-      })
-      .catch((error) => {
-        console.log(error);
-        console.log("weekly prices not retrieved");
-      });
-  }
-
-  async function dataFetchCurrent() {
-    await axios
-      .get(endpoints.current)
-      .then((response) => {
-        setPrice(
-          response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
-        );
-      })
-      .then(console.log("this is price useState", price))
-      .catch((error) => {
-        console.log(error);
-        console.log("current price not retrieved");
-      });
-  }
-
+  // const [month, setMonth] = useState();
+  // const [monthPrice, setMonthPrice] = useState();
   useEffect(() => {
-    dataFetchCurrent();
+    async function dataFetchWeekly() {
+      await axios
+        .get(endpoints.weekly)
+        .then((response) => {
+          console.log(
+            "this response.data",
+            response.data["Time Series (Digital Currency Weekly)"]
+          );
+          let data = response.data;
+          console.log("this is data", data);
+          setWeeklyPriceData(data["Time Series (Digital Currency Weekly)"]);
+          console.log("this is usestate variable", weeklyPriceData);
+
+          let dates = Object.keys(
+            response.data["Time Series (Digital Currency Weekly)"]
+          );
+          setWeeks(dates);
+          // console.log("this is weekly data", weeklyPriceData[weeks[0]]);
+          setWeekPrices(weeklyPriceData[weeks[0]]);
+          console.log("weekly useState", weeklyPriceData[weeks[0]]);
+        })
+        .then(setLoading(false))
+        .catch((error) => {
+          console.log(error);
+          console.log("weekly prices not retrieved");
+        });
+    }
+
+    async function dataFetchCurrent() {
+      await axios
+        .get(endpoints.current)
+        .then((response) => {
+          setPrice(
+            response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+          );
+        })
+        .then(console.log("this is price useState", price))
+        .catch((error) => {
+          console.log(error);
+          console.log("current price not retrieved");
+        });
+    }
+
+    // dataFetchCurrent();
     dataFetchWeekly();
   }, []);
-
+  if (loading)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>A fun little application exploring the Alpha Vantage API</p>
+          <p>LOADING . . . </p>
+          <a
+            className="App-link"
+            href="https://www.alphavantage.co/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Check out the API here
+          </a>
+        </header>
+      </div>
+    );
   return (
     <div className="App">
       <header className="App-header">
