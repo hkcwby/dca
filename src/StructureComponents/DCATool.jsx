98ad15beb59821693f38c0de124218c390241e68
hkcwby <alexanderstevenson@me.com>
2023-02-28 14:00:54 -0600

    removed all unused code

--- src/StructureComponents/DCATool.jsx
+++ src/StructureComponents/DCATool.jsx
@@ -1,20 +1,12 @@
 import { useState } from "react";
-import { Data } from "../utils/dummyData";
 import LineChart from "./LineChart";
 
 function DCATool(props) {
   const [amount, setAmount] = useState(0);
   const [startDate, setStartDate] = useState("");
-  const [valueBitcoinAverage, setValueBitcoinAverage] = useState(0);
   const [moneyInvested, setMoneyInvested] = useState(0);
   const [valid, setValid] = useState(true);
   const [chartData, setChartData] = useState();
-
-  //currently unused price options
-  const [valueBitcoinHigh, setValueBitcoinHigh] = useState(0);
-  const [valueBitcoinLow, setValueBitcoinLow] = useState(0);
-  const [valueBitcoinOpen, setValueBitcoinOpen] = useState(0);
-  const [valueBitcoinClose, setValueBitcoinClose] = useState(0);
 
   function updateAmount(value) {
     isNaN(value) ? setValid(false) : setValid(true);
@@ -24,44 +16,14 @@ function DCATool(props) {
     const data = Object.keys(props.priceData);
     const dataSelection = data.slice(0, data.indexOf(startDate) + 1);
 
-    //gather all the opening prices
-    const openPrices = dataSelection.map(
-      (date) => props.priceData[date][`1a. open (${props.currency})`]
-    );
-
-    const bitcoinOpen = openPrices.reduce(
-      (stored, value) => stored + amount / value,
-      0
-    );
-
     //gather all the high prices
     const highPrices = dataSelection.map(
       (date) => props.priceData[date][`2a. high (${props.currency})`]
     );
 
-    const bitcoinHigh = highPrices.reduce(
-      (stored, value) => stored + amount / value,
-      0
-    );
-
     //gather all the low prices
     const lowPrices = dataSelection.map(
       (date) => props.priceData[date][`3a. low (${props.currency})`]
-    );
-
-    const bitcoinLow = lowPrices.reduce(
-      (stored, value) => stored + amount / value,
-      0
-    );
-
-    //gather all the low closing
-    const closePrices = dataSelection.map(
-      (date) => props.priceData[date][`4a. close (${props.currency})`]
-    );
-
-    const bitcoinClose = closePrices.reduce(
-      (stored, value) => stored + amount / value,
-      0
     );
 
     //gather average of low and high prices
@@ -102,28 +64,12 @@ function DCATool(props) {
         {
           label: "DCA Present Value ",
           data: chartValuesDCA,
-          //data: Data[0].map((data) => data.userGain),
-          // backgroundColor: [
-          //   "rgba(75,192,192,1)",
-          //   "#ecf0f1",
-          //   "#50AF95",
-          //   "#f3ba2f",
-          //   "#2a71d0",
-          // ],
           borderColor: "black",
           borderWidth: 2,
         },
         {
           label: "Invested Amount ",
           data: chartValuesInvest,
-          //data: Data[1].map((data) => data.userGain),
-          // backgroundColor: [
-          //   "rgba(75,192,192,1)",
-          //   "#ecf0f1",
-          //   "#50AF95",
-          //   "#f3ba2f",
-          //   "#2a71d0",
-          // ],
           borderColor: "red",
           borderWidth: 2,
         },
@@ -132,12 +78,6 @@ function DCATool(props) {
 
     //the total invested amount
     setMoneyInvested(amount * average.length);
-    //the present value of the bitcoin accumulated
-    setValueBitcoinAverage(Math.floor(bitcoinAverage * props.currentPrice));
-    setValueBitcoinHigh(Math.floor(bitcoinHigh * props.currentPrice));
-    setValueBitcoinLow(Math.floor(bitcoinLow * props.currentPrice));
-    setValueBitcoinOpen(Math.floor(bitcoinOpen * props.currentPrice));
-    setValueBitcoinClose(Math.floor(bitcoinClose * props.currentPrice));
   }
 
   return (
@@ -238,37 +178,14 @@ function DCATool(props) {
           <div>
             Present Value: {props.marker}
             {chartData
-              ? chartData.datasets[0].data[
-                  chartData.datasets[0].data.length - 1
-                ]
+              ? Math.floor(
+                  chartData.datasets[0].data[
+                    chartData.datasets[0].data.length - 1
+                  ]
+                )
               : "-"}
           </div>
-          {/* <div>
-            Present Value: {props.marker}
-            {valueBitcoinAverage ? valueBitcoinAverage : "-"}
-          </div> */}
         </div>
-        {/* <div>PV Based On Point Of Purchase:</div>
-        <div>
-          Low of each period: {props.marker}
-          {valueBitcoinLow ? valueBitcoinLow : "-"}
-        </div>
-        <div>
-          High of each period: {props.marker}
-          {valueBitcoinHigh ? valueBitcoinHigh : "-"}
-        </div>
-        <div>
-          Average of each period: {props.marker}
-          {valueBitcoinAverage ? valueBitcoinAverage : "-"}
-        </div>
-        <div>
-          Opening of each period: {props.marker}
-          {valueBitcoinOpen ? valueBitcoinOpen : "-"}
-        </div>
-        <div>
-          Close of each period: {props.marker}
-          {valueBitcoinClose ? valueBitcoinClose : "-"}
-        </div> */}
       </div>
     </div>
   );
